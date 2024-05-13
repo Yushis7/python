0dfc06c243caeb80e5d7fb162d56dbc2d98956da
Yushis7 <150568560+Yushis7@users.noreply.github.com>
2024-05-14 07:29:57 +0900

    Add files via upload

---
+++ 연습용/py16 tuple.py
@@ -0,0 +1,19 @@
+thistuple = ("apple", "banana", "cherry")
+for x in thistuple:
+  print(x)
+
+
+thistuple = ("apple", "banana", "cherry")
+for i in range(len(thistuple)):
+  print(thistuple[i])
+
+
+mytuple = ("apple", "banana", "cherry")
+for x in mytuple:
+  print(x)
+
+
+thistuple = ("apple", "banana", "cherry")
+y = ("orange",)
+thistuple += y
+print(thistuple)
